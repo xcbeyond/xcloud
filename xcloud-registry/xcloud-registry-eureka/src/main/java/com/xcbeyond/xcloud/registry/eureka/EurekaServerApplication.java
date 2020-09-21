984536d71fca204b7d47360e3e233c012cd3efb8
xcbeyond <xcbeyond157@163.com>
2020-09-21 15:07:02 +0800

    注册中心-Eureka

---
+++ xcloud-registry/xcloud-registry-eureka/src/main/java/com/xcbeyond/xcloud/registry/eureka/EurekaServerApplication.java
@@ -0,0 +1,17 @@
+package com.xcbeyond.xcloud.registry.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author xcbeyond
+ * @date 2020/9/17 0017 17:13
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerApplication.class, args);
+    }
+}
